404c50a8f9021f0a9dabc34605f37ad1df0b45fd
TaitAref <tyfalkhlyfy@gmail.com>
2026-05-10 17:19:31 +0300

    Add files via upload

---
+++ splash_screen.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() =>
+      _SplashScreenState();
+}
+
+class _SplashScreenState
+    extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+      const Duration(seconds: 3),
+          () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const HomeScreen(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF181A20),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment:
+          MainAxisAlignment.center,
+
+          children: const [
+
+            Icon(
+              Icons.account_balance_wallet,
+              size: 80,
+              color: Colors.white,
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "Student Budget",
+
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
